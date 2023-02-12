accbc326b884e0386ead17c4e925d1dce79da674
HugoLorent <hugo.lorent@outlook.com>
2023-02-12 19:36:32 +0100

    finish main script
    Signed-off-by: HugoLorent <hugo.lorent@outlook.com>

--- learn-kana.js
+++ learn-kana.js
@@ -1,3 +1,22 @@
-import * as romajiConv from '@koozaki/romaji-conv';
-console.log(romajiConv.toHiragana('ga'));
-console.log(romajiConv.toHiragana('kya'));
+import { LATIN_TO_HIRAGANA } from './latin-to-hiragana.js';
+
+const latinValuesArray = Object.keys(LATIN_TO_HIRAGANA);
+
+const generateRandomLatinValue = () => {
+  const maxLimit = Object.keys(LATIN_TO_HIRAGANA).length;
+  const rand = Math.floor(Math.random() * maxLimit);
+  return rand;
+};
+
+while (true) {
+  const randomLatinValue = latinValuesArray[generateRandomLatinValue()];
+  console.log(
+    `La lettre à écrire en Hiragana sera la suivante : ${randomLatinValue}`
+  );
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log(
+    `Voici le kana correspondant : ${LATIN_TO_HIRAGANA[randomLatinValue]}`
+  );
+  console.log('Suivant...\n');
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+}
